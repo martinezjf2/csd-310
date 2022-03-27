726546b9ef4fb2aec4b499d0d25cf52670d1ff88
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-27 19:16:48 -0400

    added steps pseudocode to assignment_2 in module 2

---
+++ module_2/assignment_2.java
@@ -0,0 +1,22 @@
+// Write a program that calculates the energy needed to heat water from an initial temperature to a final temperature. Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperature of the water.
+
+
+// Q = waterMass ( finalTemperature – initialTemperature ) x 4184
+// waterMass is water weight in kilograms
+// finalTemperature and initialTemperature are temperatures in Celsius
+// Q is the results in Joules
+
+
+// Step 1: Prompt the User to enter amount of water in kilograms(watermass)
+
+// Step 2: Prompt the User to enter initial temp. of the water
+
+// Step 3: Prompt the User to enter the final temp. of the water
+
+// Step 4: Calculate 
+// Q = watermass(kg) ( finalTemperature - initialTemperature ) * 4184
+
+// Step 5: Give the output of the calculation 
+
+// Step 6: Exit the program
+
