cfdc92726480b0e964b58e05b4b79c31a5ff49aa
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-27 19:30:29 -0400

    added more notes, imported library, asked for user input

--- module_2/main.java
+++ module_2/main.java
@@ -6,9 +6,20 @@
 // finalTemperature and initialTemperature are temperatures in Celsius
 // Q is the results in Joules
 
+// Step 0: import Scanner library
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+// Step #: set Scanner library to a variable
+Scanner input = new Scanner(System.in);
 
 // Step 1: Prompt the User to enter amount of water in kilograms(watermass)
-
+System.out.print("Enter kilograms to the nearest decimal: ");
+float kgFloat = input.nextFloat();
+System.out.println("You've Entered: " + kgFloat);
 // Step 2: Prompt the User to enter initial temp. of the water
 
 // Step 3: Prompt the User to enter the final temp. of the water
@@ -19,4 +30,7 @@
 // Step 5: Give the output of the calculation 
 
 // Step 6: Exit the program
-
+// Step #: Close the input
+input.close();
+    }
+}
