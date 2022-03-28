3c8c1e17914cc66bf393b1fa60f30eebb6d5be32
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-27 20:03:52 -0400

    finished assignment

--- module_2/main.java
+++ module_2/main.java
@@ -1,3 +1,6 @@
+
+
+//Objective
 // Write a program that calculates the energy needed to heat water from an initial temperature to a final temperature. Your program should prompt the user to enter the amount of water in kilograms and the initial and final temperature of the water.
 
 
@@ -6,49 +9,69 @@
 // finalTemperature and initialTemperature are temperatures in Celsius
 // Q is the results in Joules
 
-// Step 0: import Scanner library
+
+
+
+
+
+
+
+
+
+// Step 1: import Scanner library
 import java.util.Scanner;
 
+// Step 2: Initialize Class
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-// Step #: set Scanner library to a variable
+// Step 3: Greet the User
+        System.out.println("\n\nWelcome to the Joules Calculator! \n");
+
+// Step 4: Set Scanner library to a variable
 Scanner input = new Scanner(System.in);
 
-// Step 1: Prompt the User to enter amount of water in kilograms(watermass)
-
+// Step 5: Prompt the User to enter amount of water in kilograms(watermass)
 System.out.print("\nEnter kilograms to the nearest decimal: ");
 float kgFloat = input.nextFloat();
-System.out.println("\nYou've Entered: " + kgFloat);
+// System.out.println("\nYou've Entered: " + kgFloat);
 
-// Step 2: Prompt the User to enter initial temp. of the water
-System.out.print("Enter initial temperature to the nearest decimal in Celsius: ");
+// Step 6: Prompt the User to enter initial temp. of the water
+System.out.print("\nEnter initial temperature to the nearest decimal in Celsius: ");
 float initialTemperature = input.nextFloat();
-System.out.println("\nYou've Entered: " + initialTemperature);
+// System.out.println("\nYou've Entered: " + initialTemperature);
 
 
-// Step 3: Prompt the User to enter the final temp. of the water
-System.out.print("Enter final temperature to the nearest decimal in Celsius: ");
+// Step 7: Prompt the User to enter the final temp. of the water
+System.out.print("\nEnter final temperature to the nearest decimal in Celsius: ");
 float finalTemperature = input.nextFloat();
-System.out.println("\nYou've Entered: " + finalTemperature);
+// System.out.println("\nYou've Entered: " + finalTemperature);
 
-// Step 4: Calculate 
+
+// Step 8: Calculate 
 // Q = watermass(kg) ( finalTemperature - initialTemperature ) * 4184
-
 float temperature = finalTemperature - initialTemperature;
-System.out.println("\nTemperature: " + temperature);
+//System.out.println("\nTemperature: " + temperature);
+float total = kgFloat * temperature * 4184;
+// System.out.println("\nTotal: " + total);
 
-// Step 5: Give the output of the calculation 
-System.out.print("\nEnter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-System.out.print("Enter final temperature to the nearest decimal in Celsius: \n");
-// Step 6: Exit the program
-// Step #: Close the input
+
+
+// Step 9: Give the output of the calculation 
+System.out.print("\n---------------------------------------------------------- \n");
+System.out.print("\n                      Calculations                       \n");
+System.out.print("\nWatermass in kg     : " + kgFloat + " kg" + "\n");
+System.out.print("Initial Temperature : " + initialTemperature + " degrees Celsius" + "\n");
+System.out.print("Final Temperature   : " + finalTemperature + " degrees Celsius" + "\n");
+System.out.print("\nTotal               : " + total + " Joules" + "\n");
+System.out.print("\n---------------------------------------------------------- \n\n\n");
+
+
+// Step 10: Close the Scanner Libaray with the input variable
 input.close();
     }
 }
+
+
+//Step 11: Add resources used 
+//Resources used: https://www.programiz.com/java-programming/operators
