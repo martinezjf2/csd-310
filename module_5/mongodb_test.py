f6b0a2bc4e3afe24b19978e62d2bda83a88b22ce
Jeffrey Martinez <martinezjf2@gmail.com>
2022-04-09 11:43:03 -0400

    made successful connection with mongdb

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,7 @@
+
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.oe6fd.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
